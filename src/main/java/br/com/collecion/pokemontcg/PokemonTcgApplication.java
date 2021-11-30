a8715a796083abb83dc87ba8d9016c61b8670d27
Douglas Dreer <douglasdreer@outlook.com.br>
2021-11-30 13:25:30 -0300

    For each push on the master branch will run sonarqube

---
+++ src/main/java/br/com/collecion/pokemontcg/PokemonTcgApplication.java
@@ -0,0 +1,13 @@
+package br.com.collecion.pokemontcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTcgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonTcgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/collecion/pokemontcg/PokemonTcgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.collecion.pokemontcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
