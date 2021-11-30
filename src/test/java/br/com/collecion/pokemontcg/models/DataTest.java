025fd72e4d719f6b5f0d61785acb0eb96a49e3af
Douglas Dreer <douglasdreer@outlook.com.br>
2021-11-30 16:54:56 -0300

    added unit tests

--- src/test/java/br/com/collecion/pokemontcg/models/DataTest.java
+++ src/test/java/br/com/collecion/pokemontcg/models/DataTest.java
@@ -1,22 +1,42 @@
 package br.com.collecion.pokemontcg.models;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DataTest implements Models{
-    private static final String name = "name";
-    private static final String type = "type";
-    private static final String text = "text";
+    private static final Card card = new Card();
+
+    @BeforeEach
+    public void setup() {
+        card.setId("1");
+    }
 
     @Override
     public void mustReturnSuccess_WhenCreateByConstructorWithParams() {
-
+        Data data = new Data(card);
+        check(data);
     }
 
     @Override
     public void mustReturnSuccess_WhenCreateBySetters() {
-
+        Data data = new Data();
+        data.setData(card);
+        check(data);
     }
 
     @Override
     public void mustReturnSuccess_WhenCreateByBuilder() {
+        Data data = Data.builder()
+                .data(card)
+                .build();
+        check(data);
+    }
 
+    private void check(Data item){
+        assertNotNull(item, createMsg("Data"));
+        assertEquals(card, item.getData(), createMsg("card"));
+        assertNotNull(item.getData());
     }
 }
