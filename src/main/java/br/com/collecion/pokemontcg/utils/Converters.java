e03f38e768da239e593234491955ac221b6a2557
Douglas Dreer <douglasdreer@outlook.com.br>
2021-12-10 00:58:20 -0300

    Added a utility class to convert from DTO to Entity and vice versa

---
+++ src/main/java/br/com/collecion/pokemontcg/utils/Converters.java
@@ -0,0 +1,32 @@
+package br.com.collecion.pokemontcg.utils;
+
+import br.com.collecion.pokemontcg.dtos.UserDTO;
+import br.com.collecion.pokemontcg.enities.User;
+
+public class Converters {
+    public static User UserDTOoUserEntity(UserDTO dto){
+        return User.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .createAt(dto.getCreateAt())
+                .updateAt(dto.getUpdateAt())
+                .status(dto.getStatus())
+                .build();
+    }
+
+    public static UserDTO UserEntityToUserDTO(User entity){
+        return UserDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .email(entity.getEmail())
+                .username(entity.getUsername())
+                .password(entity.getPassword())
+                .createAt(entity.getCreateAt())
+                .updateAt(entity.getUpdateAt())
+                .status(entity.getStatus())
+                .build();
+    }
+}
